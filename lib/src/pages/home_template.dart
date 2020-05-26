9f4cf6ebaf7eab750d2038c66df2c466568ab841
Matias Doto <matydoto@gmail.com>
2020-05-26 04:35:11 -0300

    listView

---
+++ lib/src/pages/home_template.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+
+  final List<String> opciones = ['uno', 'dos', 'tres', 'cuatro', 'cinco'];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        children: _crearItemsWithMap(),
+      ),
+    );
+  }
+
+  List<Widget> _crearItems() {
+
+    List<Widget> lista = new List<Widget>();
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista..add(tempWidget)
+          ..add(Divider());
+    }
+    return lista;
+
+  }
+
+
+  List<Widget> _crearItemsWithMap() {
+
+    return opciones.map((opt) {
+      return Column(
+        children: <Widget>[
+          ListTile(
+            title: Text('$opt!'),
+            subtitle: Text('un subtítulo...'),
+            leading: Icon(Icons.account_balance_wallet),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
+
+  }
+
+}
